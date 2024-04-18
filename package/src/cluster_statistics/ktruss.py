d32eacd52a3dc755036687151aac55b0ca2a7f34
Fabio Ayres <fabioja@insper.edu.br>
2024-04-18 17:16:19 -0300

    packaging stats code

--- package/src/cluster_statistics/ktruss.py
+++ package/src/cluster_statistics/ktruss.py
@@ -1,14 +1,16 @@
+# pylint: disable=missing-docstring
 import networkx as nx
 
-def find_max_k_truss(G):
+
+def find_max_k_truss(graph):
     # initialize the bounds of the binary search
     left = 2
-    right = max(d for n, d in G.degree())
+    right = max(d for _, d in graph.degree())
 
     # perform the binary search
     while left <= right:
         mid = (left + right) // 2
-        k_truss = nx.k_truss(G, k=mid)
+        k_truss = nx.k_truss(graph, k=mid)
         if k_truss:
             # if the k-truss is not empty, try increasing k
             left = mid + 1
